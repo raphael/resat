08f9ac978e319d88a9a9e3250f581bdc5bdb2115
deborah <deborah@rightscale.com>
2009-06-11 11:02:09 -0700

    added ability to use status for guards

--- lib/guard.rb
+++ lib/guard.rb
@@ -16,15 +16,24 @@ module Resat
       @timeout ||= 120
       @period ||= 5
       @failures = []
-      
-      Variables.substitute!(@pattern)
-      r = Regexp.new(@pattern)
-      r.match(request.get_response_field(@field, @target))
-      expiration = DateTime.now + @timeout
-      while !Regexp.last_match && DateTime.now < expiration && request.failures.empty?
-        sleep @period
-        request.send
+
+      if @field
+        Variables.substitute!(@pattern)
+        r = Regexp.new(@pattern)
         r.match(request.get_response_field(@field, @target))
+        expiration = DateTime.now + @timeout
+        while !Regexp.last_match && DateTime.now < expiration && request.failures.empty?
+          sleep @period
+          request.send
+          r.match(request.get_response_field(@field, @target))
+        end
+      elseif @status
+        status = nil
+        while status != @status && DateTime.now < expiration && request.failures.empty?
+          sleep @period
+          request.send
+          status = request.response.status
+        end
       end
       @failures << "Guard '#{@name}' timed out waiting for field '#{@field}' with pattern '#{@pattern ? @pattern : '<NONE>'}' from response #{@target}." if !Regexp.last_match
     end
